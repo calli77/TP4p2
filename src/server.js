b97a2cc6ed36b6d4a87cdd8934e05f489266f007
calli77 <calliclesbazolo@gmail.com>
2024-02-06 02:49:30 +0100

    Etape 4

--- src/server.js
+++ src/server.js
@@ -1,3 +1,33 @@
-import {getData} from "./api.js";
+import fastify from 'fastify';
+import fastifyView from '@fastify/view';
+import handlebars from 'handlebars';
+import { getData } from "./api.js";
 
-console.log(await getData("https://gateway.marvel.com:443"));
+
+const app = fastify();
+app.register(fastifyView, {
+    engine: {
+        handlebars,
+    },
+    templates: "./templates",
+    options: {
+        partials: {
+            header: 'header.hbs',
+            footer: 'footer.hbs'
+        }
+    },
+});
+
+app.get('/characters', async (req, res) => {
+    try {
+        const characters = await getData('https://gateway.marvel.com');
+        await res.view('index.hbs', { characters });
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
